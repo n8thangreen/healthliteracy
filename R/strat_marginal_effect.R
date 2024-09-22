b0cd9a04ac10fcd447b79d1b50f58fc2c34ff58b
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2024-09-22 20:28:11 +0100

    saved outputs so can just read this in to the dashboard
    * need to fix bug with strat_marginal_effect() for gross_income. This may be to do with more than 2 levels?
    
    * replicate for stan models too

--- R/strat_marginal_effect.R
+++ R/strat_marginal_effect.R
@@ -2,15 +2,14 @@
 #' @title Stratified Marginal Effect
 #'
 #' @param fit glm or stan model output
-#' @param data data frame
+#' @param survey_dat data frame
 #' @param interaction Name of the interaction term
 #' @return List
 #'
-strat_marginal_effect <- function(fit, data, interaction = "workingstatus") {
+strat_marginal_effect <- function(fit, survey_dat, mrp_dat, interaction = "workingstatus") {
 
   names_vars <- all.vars(terms(fit)[[3]])
-
-  response <- all.vars(terms(fit_freq[[1]]))[1]
+  response <- all.vars(terms(fit))[1]
 
   main_vars <- names_vars[names_vars != interaction]
   main_term <- paste(main_vars, collapse = " + ")
@@ -18,16 +17,15 @@ strat_marginal_effect <- function(fit, data, interaction = "workingstatus") {
   # formula with interaction term
   rhs <- glue::glue("1 + {interaction} * ({main_term})")
 
-  fit <- glm(glue("{response} ~ {rhs}"), data = data,
+  fit <- glm(glue("{response} ~ {rhs}"), data = survey_dat,
              family = binomial(), weights = weights)
 
   ame_dat <- list()
   ps_strat <- list()
 
   for (i in names_vars) {
-    for (j in levels(data[[interaction]])) {
-
-      strat_dat <- filter(data, {{interaction}} == j)
+    for (j in levels(mrp_dat[[interaction]])) {
+      strat_dat <- filter(mrp_dat, !!sym(interaction) == j)
       strat_dat$predicted_prob <- predict(fit, strat_dat, type = 'response')
 
       # total
@@ -48,9 +46,7 @@ strat_marginal_effect <- function(fit, data, interaction = "workingstatus") {
         summarize(estimate = weighted.mean(predicted_prob, product_p)) |>
         mutate(level = j)
     }
-
     ame_dat[[i]] <- bind_rows(ps_strat)
-    ame_dat[[i]]$ame <- ame_dat[[i]]$estimate - poststrat_estimates
 
     ame_dat[[i]] <-
       ame_dat[[i]] |>
@@ -60,5 +56,7 @@ strat_marginal_effect <- function(fit, data, interaction = "workingstatus") {
     # common first column name
     names(ame_dat[[i]])[1] <- "name"
   }
+
+  ame_dat
 }
 
--- scripts/MRP_analysis_with_functions.R
+++ scripts/MRP_analysis_with_functions.R
@@ -21,8 +21,24 @@ save(mrp_data, file = here::here("data/mrp_data.RData"))
 poststratification(fit_freq$lit_glm,
                    mrp_data$lit_dat)
 
-average_marginal_effect(fit_freq$lit_glm,
-                        mrp_data$lit_dat)
+ame_data <-
+  average_marginal_effect(fit_freq$lit_glm,
+                          mrp_data$lit_dat)
 
-strat_marginal_effect(fit_freq$lit_glm,
-                      mrp_data$lit_dat)
+save(ame_data, file = here::here("data/ame_data.RData"))
+
+# stratified average marginal effect
+
+names_vars <- all.vars(terms(fit_freq[[1]])[[3]])
+stat_ame_data <- list()
+
+##TODO: fix error for gross_income
+for (i in names_vars) {
+  stat_ame_data[[i]] <-
+    strat_marginal_effect(fit_freq$lit_glm,
+                          survey_data$lit_dat,
+                          mrp_data$lit_dat,
+                          interaction = i)
+}
+
+save(stat_ame_data, file = here::here("data/stat_ame_data.RData"))
