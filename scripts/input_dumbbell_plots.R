8d3f504b3070d714493417abc3ad0246245658dd
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2025-03-21 08:29:01 +0000

    input dumbbell plot created

---
+++ scripts/input_dumbbell_plots.R
@@ -0,0 +1,34 @@
+# dumbbell plots
+#
+#https://r-charts.com/distribution/dumbbell-plot-ggplot2/
+
+library(ggalt)
+
+plot_dat <- data.frame(var = full_table$Variable,
+                       cat = full_table$Category,
+                       sfl = as.numeric(full_table[, 4]),
+                       pop = as.numeric(full_table[, 10]))
+
+plot_dat <- plot_dat |>
+  mutate(pop = ifelse(var == "Gross Income (£)" & cat == "Other", 0, pop),
+         pop = ifelse(var == "IMD (decile)" & cat == "7", 0, pop),
+         pop = ifelse(var == "IMD (decile)" & cat == "9", 0, pop))
+
+# downward fill empty variable rows
+plot_dat <- plot_dat %>%
+  mutate(var = na_if(var, "")) %>%
+  tidyr::fill(var, .direction = "down")
+
+p <-
+  ggplot(data = plot_dat, aes(y = cat, x = sfl, xend = pop)) +
+  geom_dumbbell(size_x = 3,
+                size_xend = 3,
+                colour_x = "#F69541",
+                colour_xend = "#699DC6") +
+  # Plot points separately to handle missing data
+  geom_point(aes(x = sfl), size = 3, colour = "#F69541", na.rm = TRUE) +
+  geom_point(aes(x = pop), size = 3, colour = "#699DC6", na.rm = TRUE) +
+  ylab("") + xlab("") + theme_bw() + coord_flip() +
+  facet_wrap(~ var, scales = "free_x")
+
+ggsave("plots/dumbbell_plot.png", p, width = 10, height = 10, units = "in", dpi = 300)
