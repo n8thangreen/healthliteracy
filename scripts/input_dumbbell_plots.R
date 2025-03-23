b5297ab892fa094622a9ac0594ec317afcb96fb7
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2025-03-23 13:26:26 +0000

    fixed problem with same coord for both points so overlappin g by using a hollow point

--- scripts/input_dumbbell_plots.R
+++ scripts/input_dumbbell_plots.R
@@ -5,9 +5,9 @@
 library(ggalt)
 
 # select
-output <- litaracy
-output <- numeracy
-output <- ICT
+# output <- "literacy"
+# output <- "numeracy"
+output <- "ICT"
 
 colnum <- ifelse(
   output == "literacy", 4,
@@ -19,9 +19,12 @@ plot_dat <- data.frame(var = full_table$Variable,
                        pop = as.numeric(full_table[, 10]))
 
 plot_dat <- plot_dat |>
-  mutate(pop = ifelse(var == "Gross Income (£)" & cat == "Other", 0, pop),
-         pop = ifelse(var == "IMD (decile)" & cat == "7", 0, pop),
-         pop = ifelse(var == "IMD (decile)" & cat == "9", 0, pop))
+  mutate(pop = ifelse(is.na(pop), 0, pop))
+
+# plot_dat <- plot_dat |>
+#   mutate(pop = ifelse(var == "Gross Income (£)" & cat == "Other", 0, pop),
+#          pop = ifelse(var == "IMD (decile)" & cat == "7", 0, pop),
+#          pop = ifelse(var == "IMD (decile)" & cat == "9", 0, pop))
 
 # downward fill empty variable rows
 plot_dat <- plot_dat %>%
@@ -30,13 +33,14 @@ plot_dat <- plot_dat %>%
 
 p <-
   ggplot(data = plot_dat, aes(y = cat, x = sfl, xend = pop)) +
-  geom_dumbbell(size_x = 3,
-                size_xend = 3,
-                colour_x = "#F69541",
-                colour_xend = "#699DC6") +
+  geom_dumbbell() +
+  # geom_dumbbell(size_x = 3,
+  #               size_xend = 3,
+  #               colour_x = "#F69541",
+  #               colour_xend = "#699DC6") +
   # Plot points separately to handle missing data
-  geom_point(aes(x = sfl), size = 3, colour = "#F69541", na.rm = TRUE) +
-  geom_point(aes(x = pop), size = 3, colour = "#699DC6", na.rm = TRUE) +
+  geom_point(aes(x = sfl), size = 4, colour = "#F69541", na.rm = TRUE) +
+  geom_point(aes(x = pop), size = 3, shape = 1, stroke = 2, colour = "#699DC6", na.rm = TRUE) +
   ylab("") + xlab("") + theme_bw() + coord_flip() +
   facet_wrap(~ var, scales = "free_x")
 
