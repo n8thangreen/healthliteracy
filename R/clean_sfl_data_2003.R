811b59fe3c12cdf6843e8a2d5f42c34886036e21
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2025-10-10 17:22:00 +0100

    add SfL 2003 data set and start to draft cleaning function corresponding to SfL 2011 data

---
+++ R/clean_sfl_data_2003.R
@@ -0,0 +1,148 @@
+
+#' @title Clean skills for life survey 2003 data
+#'
+#' Following Rowlands paper, we create derived variables and reorder
+#'
+#' @param data Survey data at individual level i.e. raw Skills for Life data
+#' @param save Logical indicating whether to save the cleaned data sets
+#' @return List of cleaned data sets for literacy, ict and numeracy
+#'
+#' @import dplyr
+#' @importFrom tibble lst
+#'
+clean_sfl_data_2003 <- function(data, save = FALSE) {
+
+  # select variables
+  model_dat <-
+    data |>
+    dplyr::select(
+      EMPSTAT,  #WORKINGSTATUS2,     # 724 - Whether working (paid work, government training scheme, own business, or unpaid work for relatives)
+      EARNING,  #GROSS_ANNUAL_INCOME_OLDBANDS,  # 815 - Banded annual gross income for employees and self-employed (banded in line with 2003)
+      #missing  # BUK,                # 19 - Whether born in the UK
+      TENURE,  #QxTenu1,            # 763 - Home ownership status
+      SEX,  #Sex1,               # 6 -Gender (Respondent)
+      AGEBAND,  #AGE1NET,            # 11 - Age of the respondent (3 band nets)
+      ENGSTAT,  #Sesol,              # is English first language
+      ETHNIC,  #ETHNICSIMPLE,       # 17 - Simple ethnic group identifier
+      HIQUAG1,   #HIQUAL,             # 581 - Highest qualification currently held
+      IMDSCOREB4,  #IMDSCOREB4,         # Index of Multiple Deprivation banded into deciles
+      RNSSEC,  #NSSEC7,             # 828	- NS SEC respondent - current/most recent occupation - 7 groups
+      # CLITSPEAK,          # ENFL everyday English skills (literacy and speaking)
+
+      # outcomes
+      ITSKILL,  #SUMMARYCOMP,        # self-assessed computer skills (summary)
+      ICTPRAC,  #TSKILLA,            # self-assessed computer skills (summary 2)
+      READING,  #COMBLIT,            # self-assessed reading a writing (summary)
+      WRITING,
+      LITERACYSCORENET,  #LiteracyScoreA_1,                  # literacy level
+      LITERACYTHRESHOLD,   #starts_with("LiteracyThreshold"),  # literacy threshold
+      LITLEV,
+      NUMERACYSCORENET,  #NumeracyScoreA_1,                  # numeracy level
+      NUMERACYTHRESHOLD,  #starts_with("NumeracyThreshold"),  # numeracy threshold,
+      NUMLEV,
+      # MultipleChoiceLevelA_1,            # ICT level
+      # MultipleChoiceLevelA_1Thres,       # ICT threshold
+
+      # weights
+      POPWTALL, or WTALL,  #rimweight2003,
+      POPWTLIT, or WTLIT,  #rimweightLIT2003,
+      POPWTNUM, or WTNUM,  #rimweightNUM2003,
+      # rimweightICT2003,
+      # rimweightNUMICT2003,
+      # rimweightLITICT2003,
+      POPWTBOTH, or WTBOTH  #rimweightLITNUM2003
+    ) |>
+
+    # remove class
+    dplyr::mutate(
+      WORKINGSTATUS2 = unclass(WORKINGSTATUS2),
+      GROSS_ANNUAL_INCOME_OLDBANDS = unclass(GROSS_ANNUAL_INCOME_OLDBANDS),
+      BUK = unclass(BUK),
+      QxTenu1 = unclass(QxTenu1),
+      Sex1 = unclass(Sex1),
+      AGE1NET = unclass(AGE1NET),
+      Sesol = unclass(Sesol),
+      ETHNICSIMPLE = unclass(ETHNICSIMPLE),
+      HIQUAL = unclass(HIQUAL),
+      IMDSCOREB4 = unclass(IMDSCOREB4),
+      NSSEC7 = unclass(NSSEC7),
+      LiteracyThresholdA_1 = unclass(LiteracyThresholdA_1),
+      NumeracyThresholdA_1 = unclass(NumeracyThresholdA_1),
+      MultipleChoiceLevelA_1Thres = unclass(MultipleChoiceLevelA_1Thres),
+      LiteracyScoreA_1 = unclass(LiteracyScoreA_1),
+      NumeracyScoreA_1 = unclass(NumeracyScoreA_1)) |>
+
+    # relabel and order levels
+    dplyr::transmute(
+      workingstatus = factor(WORKINGSTATUS2,
+                             levels = c(0,1), labels = c("No", "Yes")),
+      gross_income =
+        ifelse(GROSS_ANNUAL_INCOME_OLDBANDS %in% 1:2,
+               "<10000",
+               ifelse(GROSS_ANNUAL_INCOME_OLDBANDS %in% 3:6,
+                      ">=10000", "other")) |>
+        factor(levels = c("<10000", ">=10000", "other")),
+      uk_born = factor(BUK, levels = c(2,1), labels = c("No", "Yes")),
+      sex = factor(Sex1, levels = c(2,1), c("Female", "Male")),
+      own_home = ifelse(QxTenu1 == 1, "Yes", "No") |>   # assume shared ownership is not own home
+        factor(levels = c("No", "Yes")),
+      age = ifelse(AGE1NET %in% 1:2, "16-44",
+                   ifelse(AGE1NET == 3, ">=45", "other")) |>
+        factor(levels = c("16-44", ">=45")),
+      english_lang = factor(Sesol, levels = c(2,1), labels = c("No", "Yes")),
+      ethnicity = factor(ETHNICSIMPLE, levels = c(1,2), labels = c("White", "BME")),
+      qualification = ifelse(HIQUAL %in% 1:4, ">=level 2", "<=Level 1") |>
+        factor(levels = c("<=Level 1", ">=level 2")),
+      # imd = factor(10 - IMDSCOREB4),     # original greater deprivation being higher number
+      imd = case_when(
+        IMDSCOREB4 %in% c(1, 2) ~ 5,
+        IMDSCOREB4 %in% c(3, 4) ~ 4,
+        IMDSCOREB4 %in% c(5, 6) ~ 3,
+        IMDSCOREB4 %in% c(7, 8) ~ 2,
+        IMDSCOREB4 == 9         ~ 1),
+      job_status = ifelse(NSSEC7 %in% 1:2, "higher",  # managerial
+                          ifelse(NSSEC7 == 3, "intermediate",
+                                 ifelse(NSSEC7 %in% 4:10, "lower", "other"))) |>
+        factor(levels = c("lower", "intermediate", "higher")),
+      lit_thresholdL1 =
+        ifelse(LiteracyThresholdA_1 == 1, "below",
+               ifelse(LiteracyThresholdA_1 == 2, "above", "other")),
+      lit_thresholdL2 = ifelse(LiteracyScoreA_1 == 5, "above",
+                               ifelse(LiteracyScoreA_1 %in% 1:4, "below", "other")),   # >= L2
+      num_thresholdEL3 =
+        ifelse(NumeracyThresholdA_1 == 1, "below",
+               ifelse(NumeracyThresholdA_1 == 2, "above", "other")),
+      num_thresholdL1 = ifelse(NumeracyScoreA_1 == 4:5, "above",
+                               ifelse(NumeracyScoreA_1 %in% 1:3, "below", "other")),  # >= L1
+      ict_thresholdEL3 =
+        ifelse(MultipleChoiceLevelA_1Thres == 1, "below",
+               ifelse(MultipleChoiceLevelA_1Thres == 2, "above", "other")),
+      weights = unclass(rimweight2003),
+      lit_weightsL1 = unclass(rimweightLIT2003),
+      num_weightsEL3 = unclass(rimweightNUM2003),
+      ict_weightsEL3 = unclass(rimweightICT2003)) |>
+
+    # remove missing
+    dplyr::filter(!is.na(age),
+                  !is.na(ethnicity))
+
+  # health literacy assessment specific data sets
+  # filtered by have answered question
+
+  lit <- model_dat |>
+    dplyr::filter(lit_thresholdL2 %in% c("above", "below")) |>
+    mutate(lit_thresholdL2 = as.factor(lit_thresholdL2),
+           lit_thresholdL2_bin = as.integer(lit_thresholdL2) - 1L) |>
+    select(-lit_weightsL1, -num_weightsEL3, -ict_weightsEL3,
+           -num_thresholdEL3, -num_thresholdL1, -ict_thresholdEL3)
+
+  num <- model_dat |>
+    dplyr::filter(num_thresholdL1 %in% c("above", "below")) |>
+    mutate(num_thresholdL1 = as.factor(num_thresholdL1),
+           num_thresholdL1_bin = as.integer(num_thresholdL1) - 1L) |>
+    select(-lit_weightsL1, -num_weightsEL3, -ict_weightsEL3,
+           -num_thresholdEL3, -lit_thresholdL2, -ict_thresholdEL3)
+
+  tibble::lst(lit, num)
+}
+
---
+++ scripts/compare-SfL-2003-with-2011.R
@@ -0,0 +1,38 @@
+# compare SfL 2011 with SfL 2003
+
+
+library(purrr)
+
+refit <- FALSE
+use_stan <- TRUE
+
+# SfL 2011 data
+
+load(here::here("data/skills_for_life_data.RData"))
+
+survey_data2011 <- clean_sfl_data(data)
+
+if (refit) {
+  fit2011 <- fit_models(survey_data2011, stan = use_stan)
+} else {
+  load(here::here("data/fit2011.RData"))
+}
+
+# SfL 2003 data
+
+# data2003 <-
+#   haven::read_dta(
+#     file = "../../../Newham Council Fellowship/data/skills for life survey/2003/UKDA-7239-stata9/stata9/2003_skills_for_life_datafile_anonymised.dta")
+#
+# save(data2003, file = "data/skills_for_life_2003_data.RData")
+
+load(here::here("data/skills_for_life_2003_data.RData"))
+
+survey_data2003 <- clean_sfl_data(data2003)
+
+if (refit) {
+  fit20003 <- fit_models(survey_data2003, stan = use_stan)
+} else {
+  load(here::here("data/fit2003.RData"))
+}
+
